412c6b77ed882e2644f48c31e091a38ccdfb30c9
Tittaya20 <95129311+Tittaya20@users.noreply.github.com>
2023-08-27 23:46:50 +0700

    Add files via upload

---
+++ ProductApiController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Product;
+
+class ProductApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = DB::table('products')
+        ->join('brand','products.brand_id', '=', 'brand.id')
+        ->select('products.*','brand.brand_name')
+        ->get();
+        return $products;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return Product::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    { $products = DB::table('products')
+        ->join('brand','products.brand_id', '=', 'brand.id')
+        ->select('products.*','brand.brand_name')
+        ->get();
+        return $products;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+        $product->update($request->all());
+        return $product;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Product::destroy($id);
+    }
+    public function search($name)
+    {
+        return Product::where('name','like','%'.$name.'%')->get();
+    }
+}
